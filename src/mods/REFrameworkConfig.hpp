dfa5dd73b30578bc8fe3adeb149df3017284017a
IsmaelSMR-URL <158623385+IsmaelSMR-URL@users.noreply.github.com>
2026-09-20 17:05:34 -0700

    Change menu key binding from VK_INSERT to VK_DELETE

--- src/mods/REFrameworkConfig.hpp
+++ src/mods/REFrameworkConfig.hpp
@@ -45,7 +45,7 @@ public:
     }
 
 private:
-    ModKey::Ptr m_menu_key{ ModKey::create(generate_name("MenuKey_V2"), VK_INSERT) };
+    ModKey::Ptr m_menu_key{ ModKey::create(generate_name("MenuKey_V2"), VK_DELETE) };
     ModToggle::Ptr m_menu_open{ ModToggle::create(generate_name("MenuOpen"), true) };
     ModToggle::Ptr m_remember_menu_state{ ModToggle::create(generate_name("RememberMenuState"), false) };
 #if defined(RE8) && !defined(REFRAMEWORK_UNIVERSAL)
